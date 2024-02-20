c6af1d031eb3b0d0ff3d5a619f410231aad78ab7
Misbah Khan <misbah@MacBook-Air.local>
2024-02-21 01:39:52 +0600

    pagination added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,7 +9,6 @@ app.use(express.json());
 app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.krtt2d2.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri);
 
 const client = new MongoClient(uri, {
   serverApi: {
@@ -24,11 +23,37 @@ async function run() {
     const productCollection = client.db("emajohn").collection("products");
 
     app.get("/products", async (req, res) => {
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      //   console.log(page, size);
       const query = {};
+      const cursor = productCollection.find(query);
+      const products = await cursor
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+      const count = await productCollection.estimatedDocumentCount();
+
+      res.send({ count, products });
+    });
+
+    app.post("/productsByIds", async (req, res) => {
+      const ids = req.body;
+      const objectIds = ids.map((id) => new ObjectId(id));
+      const query = { _id: { $in: objectIds } };
       const cursor = productCollection.find(query);
       const products = await cursor.toArray();
       res.send(products);
     });
+
+    // app.post("/productsByIds", async (req, res) => {
+    //   const ids = req.body;
+    //   const objectIds = ids.map((id) => ObjectId(id));
+    //   const query = { _id: { $in: objectIds } };
+    //   const cursor = productCollection.find(query);
+    //   const products = await cursor.toArray();
+    //   res.send(products);
+    // });
   } finally {
   }
 }
@@ -41,7 +66,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Ema John Running on ${port}`);
 });
-
-
-
-
